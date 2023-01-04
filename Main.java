a5e7a4bcec76c65a4105531bd8df76dfb490c3e1
ArtiomMat <artiomat13100@gmail.com>
2023-01-05 00:09:04 +0200

    more java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
-		Kernel k;
+		try {
+			new Map("42.png").save("2.png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Map.java
+++ Map.java
@@ -1,49 +1,172 @@
-public class Map {
-	private int[] pixels;
-	private int dim;
+import javax.imageio.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 
-	public int getDim() {
-		return dim;
+public class Map {
+
+	public static final int L8 = 1;
+	// In 5-6-5 bit format! Not 5-5-5, leaving a useless bit is bad for AI!
+	public static final int RGB16 = 2;
+	public static final int RGB24 = 3;
+	public static final int ARGB32 = 4;
+
+	private byte[] pixels;
+	private int pixelFormat;
+	private int width, height;
+
+	public int getPixelFormat() {
+		return pixelFormat;
 	}
 
-	public int[] getPixels() {
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public byte[] getPixels() {
 		return pixels;
 	}
 
-	public void setPixel(int x, int y, int to) {
-		if (x >= dim || y >= dim || x < 0 || y < 0)
-			return;
-		pixels[y*dim+x] = to;
-	}
+	public Map(
+		byte[] pixels, 
+		int width, 
+		int height, 
+		int pixelFormat
+	) {
+		this.width = width;
+		this.height = height;
+		this.pixelFormat = pixelFormat;
 
-	public int getPixel(int x, int y) {
-		if (x >= dim || y >= dim || x < 0 || y < 0)
-			return -1;
-		return pixels[y*dim+x];
-	}
-	
-	public Map() {
-		pixels = null;
-		dim = 0;
-	}
-
-	// NOTE: Copies _pixels
-	public Map(int[] _pixels, int _dim) {
-		dim = _dim;
-		
-		pixels = new int[dim];
-		for (int i = 0; i < dim*dim; i++) {
-			pixels[i] = _pixels[i];
+		this.pixels = new byte[width*height*pixelFormat];
+		if (pixels.length == this.pixels.length) {
+			for (int i = 0; i < this.pixels.length; i++) {
+				this.pixels[i] = pixels[i];
+			}
 		}
 	}
 
-	public Map(int _dim) {
-		dim = _dim;
-		pixels = new int[dim];
+	// Assumes pixelType=L8
+	public Map(byte[] pixels, int width, int height) {
+		this(pixels, width, height, L8);
 	}
-	
+
+	public Map(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public Map(String fp) throws IOException {
+		load(fp);
+	}
+
+	public void setPixel(int x, int y, byte[] pixel) {
+		for (int i = 0; i < pixelFormat; i++)
+			pixels[y*width+x+i] = pixel[i];
+	}
+
+	public void getPixel(int x, int y, byte[] pixel) {
+		for (int i = 0; i < pixelFormat; i++)
+			pixels[y*width+x+i] = pixel[i];
+	}
+
+	public byte[] getPixel(int x, int y) {
+		byte[] pixel = new byte[pixelFormat];
+		for (int i = 0; i < pixelFormat; i++)
+			pixels[y*width+x+i] = pixel[i];
+		return pixel;
+	}
+
 	// TODO: Implement: save, load
 
+	// Make sure the image is 1:1 ratio, otherwise it won't be accepted
+	public void load(String fp) throws IOException {
+		BufferedImage bf = ImageIO.read(new File(fp));
+
+		Raster r = bf.getData();
+		// Determining pixelFormat
+		{
+			ColorModel cm = bf.getColorModel();
+
+			boolean a = cm.hasAlpha();
+			int depth = cm.getPixelSize();
+			int colors = cm.getNumColorComponents();
+			
+			boolean success = true;
+			if (!a && depth == 8 && colors == 1)
+				pixelFormat = L8;
+			else if (!a && depth == 16 && colors == 3) {
+				// Make sure we are in 5-6-5 order
+				int[] sizes = cm.getComponentSize();
+				if (sizes[1] != 6)
+					success = false;
+				pixelFormat = RGB16;
+			}
+			else if (!a && depth == 24 && colors == 3)
+				pixelFormat = RGB24;
+			else if (a && depth == 32 && colors == 3) {
+				pixelFormat = ARGB32;
+				System.out.println("HAS ALPHA :O");
+			}
+			else
+				success = false;
+			
+			if (!success) {
+				throw new IOException(
+					"Map.load(): Unsupported pixel format. Check Map's formats at the top of the class definition."
+				);
+			}
+		}
+		
+		width = r.getWidth();
+		height = r.getHeight();
+		
+		pixels = new byte[width*height*pixelFormat];
+		
+		/*byte[] rBuf = ((DataBufferByte) r.getDataBuffer()).getData();
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = rBuf[i];
+		}*/
+
+		byte[] pixel = new byte[pixelFormat];
+		for (int i )
+	}
+
+	public void save(String fp) throws IOException {
+		int type;
+		if (pixelFormat == L8)
+			type = BufferedImage.TYPE_BYTE_GRAY;
+		else if (pixelFormat == RGB16)
+			type = BufferedImage.TYPE_USHORT_565_RGB;
+		else if (pixelFormat == RGB24)
+			type = BufferedImage.TYPE_INT_RGB;
+		else
+			type = BufferedImage.TYPE_INT_ARGB;
+		BufferedImage bi = new BufferedImage(width, height, type);
+
+		for(int x = 0; x < width; x++) {
+			for(int y = 0; y < height; y++) {
+				byte[] pixel = getPixel(x, y);
+				int rgb;
+				if (pixelFormat == 3)
+					rgb = (int)pixel[1]<<16 | (int)pixel[2] << 8 | (int)pixel[3];
+				else if (pixelFormat == 3)
+					rgb = (int)pixel[0]<<16 | (int)pixel[1] << 8 | (int)pixel[2];
+				else if (pixelFormat == 2)
+					rgb = 0;// TODO
+				else
+					rgb = (int)pixel[0]<<16 | (int)pixel[0] << 8 | (int)pixel[0];
+				
+				bi.setRGB(x, y, rgb);
+			}
+		}
+		
+		ImageIO.write(bi, "PNG", new File(fp));
+	}
+/*
 	public int placeOn(Map on, int _x, int _y) {
 		int ret = 0;
 		for (int x = _x; x < _x+dim; x++) {
@@ -118,5 +241,5 @@ public class Map {
 	public Map poolMin(int dimPool, int stride) {
 		return poolMinMax(dimPool, stride, false);
 	}
-
+ */
 }
